37075925b83f5dc46c89a7245a7c7ec20c247459
tanzim <syedtanzimwajih@gmail.com>
2025-12-31 01:03:43 +0530

    feat: implement UI and game reset logic

--- rockPaper.js
+++ rockPaper.js
@@ -1,69 +1,99 @@
+let buttons = document.querySelectorAll('button');
+let humanScore = 0;
+let computerScore = 0;
+let totalGames = 0;
+let gameResult = '';
+
+const result = document.querySelector('.result');
+
+const round = document.createElement('div');
+round.classList.add('round');
+round.innerText = 'Round = 0';
+result.appendChild(round);
+
+const score = document.createElement('div');
+score.classList.add('score');
+score.innerText = 'Your Score = 0 Computer Score = 0';
+result.appendChild(score);
+
+const selection = document.createElement('div');
+selection.classList.add('selection');
+selection.innerText = 'You = Null Computer = Null'
+result.appendChild(selection);
+
+const roundResult = document.createElement('div');
+roundResult.classList.add('roundResult');
+result.appendChild(roundResult);
+
+
 function getComputerChoice() {
-    let num = parseInt(Math.random() * 3);
+    let choices = ['rock', 'paper', 'scissor'];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
 
-    if (num == 0) {
-        return 'rock';
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        gameResult = 'Round draw';
     }
-    else if (num == 1) {
-        return 'paper';
+    else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore++;
+        gameResult = 'Computer won the round';
+    }
+    else if (humanChoice == 'paper' && computerChoice == 'scissor') {
+        computerScore++;
+        gameResult = 'Computer won the round';
+    }
+    else if (humanChoice == 'scissor' && computerChoice == 'rock') {
+        computerScore++;
+        gameResult = 'Computer won the round';
     }
     else {
-        return 'scissor';
+        humanScore++;
+        gameResult = 'You won the round';
     }
+};
+
+function gameReset() {
+    computerSelection = '';
+    totalGames = 0;
+    humanScore = 0;
+    computerScore = 0;
+    gameResult = '';
+
+    round.innerText = 'Round = 0';
+    score.innerText = 'Player Score = 0 Computer Score = 0';
+    selection.innerText = 'You = Null Computer = Null'
+    roundResult.innerText = '';
 }
 
-function getHumanChoice() {
-    return prompt('Enter Rock, Paper or Scissor!');
-}
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let computerSelection = getComputerChoice();
+        totalGames++;
 
+        playRound(button.id, computerSelection);
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        if(humanChoice == computerChoice){
-            console.log('round draw');
-        }
-        else if (humanChoice == 'rock' && computerChoice == 'paper') {
-            computerScore++;
-            console.log('computer won the round');
-        }
-        else if (humanChoice == 'paper' && computerChoice == 'scissor') {
-            computerScore++;
-            console.log('computer won the round');
-        }
-        else if (humanChoice == 'scissor' && computerChoice == 'rock') {
-            computerScore++;
-            console.log('computer won the round');
-        }
-        else {
-            humanScore++;
-            console.log('you won the round');
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        console.log('human selection = ', humanSelection);
-        console.log('computer selection = ', computerSelection);
+        round.innerText = `Round = ${totalGames}`;
+        score.innerText = `Your Score = ${humanScore} Computer Score = ${computerScore}`;
+        selection.innerText = `You = ${button.id} Computer = ${computerSelection}`;
+        roundResult.innerText = gameResult;
         
-        playRound(humanSelection, computerSelection);
-    }
+        if (totalGames == 5) {
+            let winner = '';
+            if (humanScore == computerScore) {
+                winner = 'Game draw';
 
-    console.log('computerScore = ', computerScore, 'human score = ', humanScore);  
+            }
+            else if (humanScore > computerScore) {
+                winner = 'You Won';
+            }
+            else {
+                winner = 'Computer won';
+            }
 
-    if(humanScore == computerScore){
-        console.log('Game draw')
-    }
-    else if (humanScore > computerScore) {
-        console.log('You won the game');
-    }
-    else {
-        console.log('computer won the game');
-    }
-}
-
-playGame();
+            alert(winner);
+            gameReset();
+        }
+    });
+});
